80fb29b5886b631cfafbdb2493db168b9f3f9cf7
Bhushan <bhushantapare201998@gmail.com>
2023-06-06 15:51:53 +0530

    java addition code

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition{
+	void add(int a, int b){
+	int c = a + b;
+		System.out.println("Add d=" +c);
+	}
+	public static void main(String[] args){
+		Addition a = new Addition();
+		a.add(10, 10);
+
+	}
+
+}
